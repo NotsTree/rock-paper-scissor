485158499e7b8f1ea07a01565ab160d3c4859747
NotsTree <88728626+NotsTree@users.noreply.github.com>
2025-04-02 15:43:19 +1000

    didnt touch html more than before, added computer choice into the js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Hello, World!");
+function getcomputerchoice(){
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+console.log(getcomputerchoice());
